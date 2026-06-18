704542d1ff378dea28bba1eb6d73c28599319c9b
Alessandro Petitcollin <atp2.7@icloud.com>
2026-06-19 00:23:48 +0200

    Test

--- headers/blabla.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLABLA_H
-#define BLABLA_H
-
-int addit(const int a, const int b);
-
-#endif // !BLABLA_H
--- src/blabla.c
+++
@@ -1,3 +0,0 @@
-#include "blabla.h"
-
-int addit(const int a, const int b) { return (a + b); }
