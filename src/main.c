d028d6047edb8e356c07fbd184b0f2ba4928a281
Madeline Wolff <git@firmaments.anonaddy.com>
2026-09-01 13:39:26 +0200

    feat(test): Adds test code.

--- src/main.c
+++ src/main.c
@@ -1,18 +1,25 @@
 #include "pinout.h"
 #include "spi.h"
 
+#include <util/delay.h>
+
 void init(void) {
   DDRB |= (CS | SCK | MOSI | MISO);
   DDRE |= (LEFT_EYE_CS | RIGHT_EYE_CS | EYES_RST);
+  DDRH |= (SCREENS_DC);
 
   PORTB |= (CS | SCK | MOSI | MISO);
   PORTE |= (LEFT_EYE_CS | RIGHT_EYE_CS | EYES_RST);
+  PORTH &= ~(SCREENS_DC);
 }
 
 int main(void) {
   init();
 
   while (1) {
-    ;
+    _delay_ms(1000);
+    PORTB ^= (CS | SCK | MOSI | MISO);
+    PORTE ^= (LEFT_EYE_CS | RIGHT_EYE_CS | EYES_RST);
+    PORTH ^= (SCREENS_DC);
   }
 }
