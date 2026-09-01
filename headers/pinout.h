a91ce5366d03097f868bf39fff8dacb0b4ff2ac0
Madeline Wolff <git@firmaments.anonaddy.com>
2026-09-01 15:17:01 +0200

    feat(headers): Updates pinout.h to add keyboard, I2C, etc. and removes masks.h

--- headers/pinout.h
+++ headers/pinout.h
@@ -1,6 +1,10 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
+// I2C GENERAL
+#define SCL (1 << PD0)
+#define SDA (1 << PD1)
+
 // SPI GENERAL
 #define CS (1 << PB0)
 #define SCK (1 << PB1)
@@ -22,5 +26,27 @@
 
 // MAIN SCREEN SPI
 #define MAIN_SCREEN_BL (1 << PH2)
+
+// KEYBOARD
+#define KB_R1 (1 << PJ2)
+#define KB_R2 (1 << PJ1)
+#define KB_R3 (1 << PJ0)
+#define KB_R4 (1 << PC7)
+
+#define KB_C1 (1 << PA3)
+#define KB_C2 (1 << PA4)
+#define KB_C3 (1 << PA5)
+#define KB_C4 (1 << PA6)
+#define KB_C5 (1 << PA7)
+#define KB_C6 (1 << PG2)
+#define KB_C7 (1 << PJ6)
+#define KB_C8 (1 << PJ5)
+#define KB_C9 (1 << PJ4)
+#define KB_C10 (1 << PJ3)
+
+// ROTARY ENCODER
+#define SDL_SW1 (1 << PC1)
+#define SDL_SW2 (1 << PC2)
+#define SDL_SW3 (1 << PC3)
 
 #endif /* ifndef PINOUT_H */
