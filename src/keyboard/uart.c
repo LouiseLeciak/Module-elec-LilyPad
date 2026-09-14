549c38bd38cdd5722e72b70505dfab3a5ae06def
LouiseLeciak <137296809+LouiseLeciak@users.noreply.github.com>
2026-09-14 10:48:58 +0200

    Lleciak.keyboard (#47)
    * feat: start of a 2x3 keyboard matrix, not working rn
    
    * add: working code for 1 button, displayread, displaywrite and pinmode are working
    
    * fix: code now works with a matrice
    
    * add: working code for 40 buttons, 4x10 matrice, one with the original pinout, one with a pinout for the arduino
    
    * add: working code for the rotary encoder, display number that increment or decrement, the button function is working
    
    * fix: not really an issue but cleaned a bit the code
    
    * add: clean code working with a menu for rotary, not i2c
    
    * fix: changed the pinout for the keyboard part
    
    * fix: changed com
    
    * add: slide switch code, white or blue led turn on, worked once and is broken now, may be a hardware issue
    
    * add: not working i2c code but good start so i keep it
    
    * fix: i2c rotary not working but not blocking everything else anymore
    
    * clean the code and add comments
    
    * fix: i2c rotary is working !!
    
    * clean proto working code
    
    * first schema for keyboard, no leds
    
    * add pin socket symbol
    
    * fix: - atmega + pinsocket in keyboard schematic
    
    * add: linked footprint
    
    * fix: schematic to 38 switchs, add: pcb placement
    
    * add: last version PCB
    
    * mid pcb commit in case of a kernel panic
    
    * check point pcb keyboard
    
    * to change: i2c to tht, else everything else is linked
    
    * add: better slide switch, have to edit vcc
    
    * fix: VCC netclass resized
    
    * quick fix
    
    * fix post Christian
    
    * add a title to keyboard pcb
    
    * add: schematic with my own symbols
    
    * clean pcb with good i2c footprint
    
    * got back the symbols, start doing the schematic with my own symbols
    
    * more footprint, started to replace in the pcb
    
    * add: footprint for slide switch and added it to pcb
    
    * add: all footprint done, pcb with own footprint, to finish
    
    * setup v2 pcb keyboard, still an issue with the rotary, misses some symbole i think
    
    * no more rotary pb
    
    * add: new male header
    
    * add: brand new pcb for the keyboard
    
    * add: symbole and footprint for diode, schematic has diode now
    
    * add: started the pcb with diode, add the 10k missing comments silkscreen
    
    * add:keyboardpcb almost done missing holes and homemade frogs
    
    * add: holes in the corner of the keyboard pcb, missing homemade frogs
    
    * maj
    
    * fix: no more dmca frog on keyboard pcb, made my own
    
    * add: the frog png
    
    * add: fix some i2c thing
    
    * fp info cache
    
    * little clean up
    
    * conflicts
    
    ---------
    
    Co-authored-by: Madeline Wolff <git@firmaments.anonaddy.com>

---
+++ src/keyboard/uart.c
@@ -0,0 +1,98 @@
+#include "utils.h"
+
+// screen /dev/ttyUSB0 115200
+// ctrl a puis k puis y
+
+void uart_init() {
+    // set baudrate
+    // p180 table 20-1
+    UBRR0H = (unsigned char)(MYUBRR >> 8);
+    UBRR0L = (unsigned char)MYUBRR;
+    // enable transmiter
+    // p202 bit 4 Reveiver enable
+    UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
+    // 8n1 => 8 bits de data, no parite, 1 bit de stop
+    // p203 20-11
+    UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00);
+
+    // p182 tab 20-1 for double speed to lower fail chance
+    UCSR0A |= (1 << U2X0);
+}
+
+void uart_tx(char c) {
+    // waiting for the buffer to be empty
+    while (!(UCSR0A & (1 << UDRE0))) {
+    }
+    // put data (char) into buffer
+    UDR0 = c;
+}
+
+char uart_rx(void) {
+    // p189, wait for data to be received
+    while (!(UCSR0A & (1 << RXC0))) {
+    }
+    // get and return received data from buffer
+    return UDR0;
+}
+
+void uart_printstr(const char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        uart_tx(str[i]);
+    }
+}
+
+void uart_printhex(uint8_t value) {
+    char hex[] = "0123456789abcdef";
+    char buf[3];
+
+    uint8_t first = value / 16;
+    uint8_t sec  = value % 16;
+
+    buf[0] = hex[first];
+    buf[1] = hex[sec];
+    buf[2] = '\0';
+
+    uart_printstr(buf);
+}
+
+uint8_t atoi_hex(char c) {
+    if (c >= '0' && c <= '9') return c - '0';
+    if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+    if (c >= 'a' && c <= 'f') return c - 'a' + 10;
+    return 0;
+}
+
+bool is_hex_char(char c) {
+    if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') ||
+        (c >= 'a' && c <= 'f'))
+        return true;
+    return false;
+}
+
+void uart_printint(int val) {
+    char buffer[10];
+    int i = 0;
+
+    // neg numbers
+    if (val < 0) {
+        uart_tx('-');
+        val = -val;
+    }
+
+    // 0
+    if (val == 0) {
+        uart_tx('0');
+        return;
+    }
+
+    // convert in string
+    while (val > 0) {
+        buffer[i++] = (val % 10) + '0';
+        val /= 10;
+    }
+
+    // put it back in the right order
+    for (int j = i - 1; j >= 0; j--) {
+        uart_tx(buffer[j]);
+    }
+}
